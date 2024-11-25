696d8ab9d7a66d964d0393d3aa1f8023f311c7da
Владимир Сулым <v.sulym@gmail.com>
2024-11-25 22:28:31 +0300

    The task with an asterisk has been completed

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 def catalog(request):
     return render(request, 'home.html')
 
 def contacts(request):
+    if request.method == "POST":
+        name = request.POST['name']
+        phone = request.POST['phone']
+        message = request.POST['message']
+        return HttpResponse(f"{name}, от Вас получено сообщений")
     return render(request, 'contacts.html')
