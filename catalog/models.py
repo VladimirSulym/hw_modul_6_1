0382fc3421a71bde1f4b73ddc760eb83ce8af5c4
Владимир Сулым <v.sulym@gmail.com>
2024-12-19 05:49:39 +0300

    created models Product and Category, created migrations and done

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Название")
+    description = models.TextField(verbose_name='Описание', blank=True, null=True)
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    price = models.FloatField()
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name_plural = 'Продукты'
+        verbose_name = 'Продукт'
+
+    def __str__(self):
+        return self.name
