01ebd30bd956379e9adc8bba3cabe6d4eb522a36
Владимир Сулым <v.sulym@gmail.com>
2024-12-19 07:26:23 +0300

    The media is set up

--- catalog/admin.py
+++ catalog/admin.py
@@ -4,8 +4,11 @@ from catalog.models import Category, Product
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description')
+    list_display = ('id', 'name',)
+    search_fields = ('name', 'description',)
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('category', 'name', 'price', 'description')
+    list_display = ('id', 'name', 'price', 'category',)
+    list_filter = ('category',)
+    search_fields = ('name', 'description',)
